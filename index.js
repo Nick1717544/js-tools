4e6af2e24632cb22c2d4e337531b2333d6bc1fa8
Nick1717544 <nickwuzhere123@gmail.com>
2022-04-10 10:39:32 +0930

    searchPosition and searchWithoutPosition
    search with and without position are completed and working along with lower and upper case the functions are pretty optimised

--- index.js
+++ index.js
@@ -35,9 +35,79 @@ class Word
             {
                 return this.word.toLowerCase().includes(letter.toLowerCase())
             }
-        }
+        } else { return 'error' }
+    }
+
+    /**
+     * 
+     * @param {string} letter 
+     * @param {number} position 
+     */
+
+    searchPosition(letter, position = 0)
+    {
+        if (position !== 0 && position <= this.word.length && letter.length > 0 && letter.length < 2)
+        {
+            if (this.caseSensitive == true)
+            {
+                const calculatedPosition = position - 1
+                return this.word[calculatedPosition] == letter
+            }
+            if (this.caseSensitive == false)
+            {
+                const calculatedPosition = position - 1
+                return this.word[calculatedPosition].toLowerCase() == letter.toLowerCase()
+            }
+        } else { return 'error' }
+    }
+
+    /**
+     * 
+     * @param {string} letter 
+     * @param {number} position 
+     */
+
+    searchWithoutPosition(letter, position = 0)
+    {
+        if (position !== 0 && position <= this.word.length && letter.length > 0 && letter.length < 2)
+        {
+            if (this.caseSensitive == true)
+            {
+                const calculatedPosition = position - 1
+                const positions = []
+                for (let i = 0; i < this.word.length; i++)
+                {
+                    if (this.word[i] == letter)
+                    {
+                        if (i !== calculatedPosition) {positions.push(i + 1)}
+                    }
+                }
+                if (positions.length == 0)
+                {
+                    return false
+                } else { return true, positions }
+            }
+            if (this.caseSensitive == false)
+            {
+                const calculatedPosition = position - 1
+                const positions = []
+                const lowerWord = this.word.toLowerCase()
+                const lowerLetter = letter.toLowerCase()
+                for (let i = 0; i < this.word.length; i++)
+                {
+                    if (lowerWord[i] == lowerLetter)
+                    {
+                        if (i !== calculatedPosition) {positions.push(i + 1)}
+                    }
+                }
+                if (positions.length == 0)
+                {
+                    return false
+                } else { return true, positions }
+            }
+        } else { return 'error' }
     }
 }
 
 const word = new Word('hello', {caseSensitive: true})
-console.log(word.search('L'));
+console.log(word.searchWithoutPosition('e', 3));
